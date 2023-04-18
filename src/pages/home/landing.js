388db9ab4546b74317d46c8a32142f21b78497ef
Andres Jimenez <andresje17@gmail.com>
2023-04-18 04:52:52 +0000

    Add home structure

---
+++ src/pages/home/landing.js
@@ -0,0 +1,13 @@
+export default function LandingPage() {
+  return (
+    <>
+      <div className="land-info">
+        <h1>Hi, I am Branskins, <br></br>a Data Enthusiast</h1>
+        <p>You can call me Andrés as well, currently working as a data engineer.
+          I spend most of my time handling big (or not) datasets,
+          and I truly enjoy getting insights from real-world data.
+        </p>
+      </div>
+    </>
+  )
+}
