064f1aa70aef0217b4205505c76187979941905c
Branskins <andresje17@gmail.com>
2023-04-19 23:22:08 -0700

    Add fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import './home/home.css'
+import { Heebo } from 'next/font/google'
+
+const heebo = Heebo({
+  subsets: ['latin'] 
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={heebo.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
