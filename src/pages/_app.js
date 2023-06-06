6ec891108a965a0ae3ab643d0bb007b10a2011de
Branskins <andresje17@gmail.com>
2023-06-05 22:09:36 -0600

    Add layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
+import Layout from './layout'
 import '@/styles/globals.css'
 import './home/home.css'
 import { Heebo } from 'next/font/google'
 
 const heebo = Heebo({
-  subsets: ['latin'] 
+  subsets: ['latin']
 })
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={heebo.className}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </main>
   )
 }
